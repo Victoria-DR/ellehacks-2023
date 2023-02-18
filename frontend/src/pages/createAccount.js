96db04d619e27baf818ffb7162b55930044bbd8e
Jacqueline Fung <fungjsl@gmail.com>
2023-02-18 11:08:03 -0500

    make all fields required

--- frontend/src/pages/createAccount.js
+++ frontend/src/pages/createAccount.js
@@ -34,6 +34,7 @@ const CreateAccount = () => {
                       name="first-name"
                       id="first-name"
                       autoComplete="given-name"
+                      required
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     />
                   </div>
@@ -66,6 +67,7 @@ const CreateAccount = () => {
                       name="email-address"
                       id="email-address"
                       autoComplete="email"
+                      required
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     />
                   </div>
