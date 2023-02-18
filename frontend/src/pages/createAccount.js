879c8bf76fea252ddc7959a0414713ea68c6a89e
Jacqueline Fung <fungjsl@gmail.com>
2023-02-18 12:18:23 -0500

    incorporate react hook form to tailwind form

--- frontend/src/pages/createAccount.js
+++ frontend/src/pages/createAccount.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useState } from "react";
 // import "../index.css";
+import { useForm } from "react-hook-form";
 
 const CreateAccount = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const [data, setData] = useState("");
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <div className="container mt-10">
       <div className="md:grid md:grid-cols-3 md:gap-6">
@@ -15,10 +25,9 @@ const CreateAccount = () => {
             </p>
           </div>
         </div>
-
         {/* Form */}
         <div className="mt-5 md:col-span-2 md:mt-0">
-          <form action="#" method="POST">
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="overflow-hidden shadow sm:rounded-md">
               <div className="bg-white px-4 py-5 sm:p-6">
                 <div className="grid grid-cols-6 gap-6">
@@ -30,15 +39,12 @@ const CreateAccount = () => {
                       First name
                     </label>
                     <input
+                      {...register("firstName")}
                       type="text"
-                      name="first-name"
-                      id="first-name"
-                      autoComplete="given-name"
                       required
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     />
                   </div>
-
                   <div className="col-span-6 sm:col-span-3">
                     <label
                       htmlFor="last-name"
@@ -47,14 +53,13 @@ const CreateAccount = () => {
                       Last name
                     </label>
                     <input
+                      {...register("lastName")}
                       type="text"
-                      name="last-name"
-                      id="last-name"
+                      required
                       autoComplete="family-name"
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     />
                   </div>
-
                   <div className="col-span-6 sm:col-span-4">
                     <label
                       htmlFor="email-address"
@@ -64,14 +69,12 @@ const CreateAccount = () => {
                     </label>
                     <input
                       type="text"
-                      name="email-address"
-                      id="email-address"
+                      {...register("email")}
                       autoComplete="email"
                       required
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     />
                   </div>
-
                   <div className="col-span-6 sm:col-span-6 lg:col-span-2">
                     <label
                       htmlFor="city"
@@ -81,22 +84,22 @@ const CreateAccount = () => {
                     </label>
                     <input
                       type="password"
-                      name="password"
-                      id="password"
+                      {...register("password")}
                       required
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     />
                   </div>
                 </div>
               </div>
-
+              <p>{data}</p>
               <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                <button
+                {/* <button
                   type="submit"
                   className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
                   Save
-                </button>
+                </button> */}
+                <input type="submit" />
               </div>
             </div>
           </form>
