dc49eb3948187dd7feda99932727d0121f73aca7
Jacqueline Fung <fungjsl@gmail.com>
2023-02-18 10:47:59 -0500

    create account page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./index.css";
+import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages";
-import About from "./pages/about";
+import CreateAccount from "./pages/createAccount";
 import Blogs from "./pages/blogs";
 import Login from "./pages/login";
 import Contact from "./pages/contact";
@@ -11,16 +12,18 @@ import PageNotFound from "./pages/pageNotFound";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/*" element={<PageNotFound />} />
-      </Routes>
-    </Router>
+    <div class="container mx-auto">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/newaccount" element={<CreateAccount />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,15 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "5rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
