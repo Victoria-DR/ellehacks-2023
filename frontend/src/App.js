75f8c0a3e198b494eed19f23f1d971d87c6d6758
Jacqueline Fung <fungjsl@gmail.com>
2023-02-18 02:52:35 -0500

    update login page name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages";
 import About from "./pages/about";
 import Blogs from "./pages/blogs";
-import SignUp from "./pages/signup";
+import Login from "./pages/login";
 import Contact from "./pages/contact";
 import PageNotFound from "./pages/pageNotFound";
 
@@ -17,7 +17,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/blogs" element={<Blogs />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </Router>
--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../index.css";
 import { LockClosedIcon } from "@heroicons/react/20/solid";
-const signup = () => {
+const login = () => {
   return (
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
@@ -103,4 +103,4 @@ const signup = () => {
   );
 };
 
-export default signup;
+export default login;
