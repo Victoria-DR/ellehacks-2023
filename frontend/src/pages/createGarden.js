9815ef923be3a44faf5e20ebc30ace64f3ee7b8a
Jacqueline Fung <fungjsl@gmail.com>
2023-02-19 05:11:03 -0500

    all routing done except navbar

--- frontend/src/pages/createGarden.js
+++ frontend/src/pages/createGarden.js
@@ -3,8 +3,10 @@ import React from "react";
 import { useState } from "react";
 // import "../index.css";
 import { useForm, Controller } from "react-hook-form";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 
 const CreateGarden = () => {
+  const navigate = useNavigate();
   const { register, control, handleSubmit, reset } = useForm();
   const [data, setData] = useState("");
 
@@ -109,12 +111,12 @@ const CreateGarden = () => {
               <p>{data}</p>
               <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
                 <button
+                  onClick={() => navigate("/garden-profile")}
                   type="submit"
                   className="inline-flex justify-center rounded-md border border-transparent bg-teal-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
                 >
                   Save
                 </button>
-                {/* <input className="submit-btn" type="submit" /> */}
               </div>
             </div>
           </form>
