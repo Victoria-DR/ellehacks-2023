7dec948e0d3a8a9d6d6f6bc3d7607a2830083144
Jacqueline Fung <fungjsl@gmail.com>
2023-02-19 06:16:57 -0500

    further improve ui

--- frontend/src/pages/Chat.js
+++ frontend/src/pages/Chat.js
@@ -60,9 +60,18 @@ const Chat = () => {
                 Response
               </label>
               <div className="mt-1">
-                <textarea className="mt-1 flex w-full justify-center rounded-md border-2 border-dashed border-gray-300 mx-auto">
-                  REPLACE THIS WITH CHATPOT STUFF
-                </textarea>
+                <div className="mt-1  flex w-full p-3 justify-center rounded-md border-2 border-dashed border-gray-300 mx-auto">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum.
+                </div>
               </div>
             </div>
           </div>
