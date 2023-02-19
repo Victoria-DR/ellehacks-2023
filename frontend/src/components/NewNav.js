6f14af4dabd7d3168be0603c2fce52b7c36bfb2b
Jacqueline Fung <fungjsl@gmail.com>
2023-02-19 07:55:02 -0500

    sign out now works

--- frontend/src/components/NewNav.js
+++ frontend/src/components/NewNav.js
@@ -124,7 +124,7 @@ export default function NewNav() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
+                            href="/"
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700"
